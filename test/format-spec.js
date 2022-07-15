702863a78583bfe6866477bfb737ab3d1406a2a5
Rebecca Vest <rvest@equinoxmedia.com>
2022-07-15 16:16:18 -0600

    Update packages and DX changes

--- test/format-spec.js
+++ test/format-spec.js
@@ -1,28 +1,33 @@
 /* global describe */
 /* global it */
-var format = require('../lib/format');
-var should = require('should');
+const format = require('../lib/format');
+// eslint-disable-next-line no-unused-vars
+const should = require('should');
 
 describe('format', function () {
   describe('getFormatString', function () {
     it('should return a format string for the requested fields and delimiters', function () {
-      var str = format.getFormatString(['hash', 'authorName'], '^', '#');
+      const str = format.getFormatString(['hash', 'authorName'], '^', '#');
+
       str.should.equal('%H^%an#');
     });
 
     it('should ignore fields that do not exist', function () {
-      var str = format.getFormatString(['abbrevHash', 'bogus', 'subject'], '*', '|');
+      const str = format.getFormatString(['abbrevHash', 'bogus', 'subject'], '*', '|');
+
       str.should.equal('%h*%s|');
     });
 
     it('should provide a default delimiter', function () {
-      var str = format.getFormatString(['hash', 'authorName']);
+      const str = format.getFormatString(['hash', 'authorName']);
+
       str.should.equal('%H' + format.FIELD_DELIMITER + '%an' + format.RECORD_DELIMITER);
     });
 
     it('should provide a default list of fields', function () {
-      var str1 = format.getFormatString(null, '^');
-      var str2 = format.getFormatString(format.DEFAULT_FIELDS, '^');
+      const str1 = format.getFormatString(null, '^');
+      const str2 = format.getFormatString(format.DEFAULT_FIELDS, '^');
+
       str1.should.equal(str2);
     });
   });
--- test/matchers-spec.js
+++ test/matchers-spec.js
@@ -1,71 +1,83 @@
 /* global describe */
 /* global it */
-var matchers = require('../lib/matchers');
-var should = require('should');
+const matchers = require('../lib/matchers');
+// eslint-disable-next-line no-unused-vars
+const should = require('should');
 
 describe('matchers', function () {
-	describe('prepareMatchers', function () {
-		it('should remove matchers that do not have a fields property', function () {
-			var m = [{
-				regexp: new RegExp('\d+')
-			}, {
-				regexp: new RegExp('\d+'),
-				fields: ['issue']
-			}];
+  describe('prepareMatchers', function () {
+    it('should remove matchers that do not have a fields property', function () {
+      const m = [
+        {
+          regexp: new RegExp('d+'),
+        },
+        {
+          regexp: new RegExp('d+'),
+          fields: ['issue'],
+        },
+      ];
 
-			var prepared = matchers.prepareMatchers(m);
-			prepared.length.should.equal(1);
-		});
-		
-		it('should remove matchers that do not have a regexp RegExp property or a pattern string property', function () {
-			var m = [{
-				pattern: '\d+',
-				fields: ['test']
-			}, {
-				regexp: new RegExp('\d+'),
-				fields: ['issue']
-			}, {
-				fields: ['issue']
-			}];
+      const prepared = matchers.prepareMatchers(m);
+      prepared.length.should.equal(1);
+    });
 
-			var prepared = matchers.prepareMatchers(m);
-			prepared.length.should.equal(2);
-		});
-		
-		it('should generate RegExp objects from a string pattern', function () {
-			var m = [{
-				pattern: '\d+',
-				fields: ['issue']
-			}];
-			
-			var prepared = matchers.prepareMatchers(m);
-			prepared[0].regexp.should.be.instanceOf(RegExp);
-		});
-		
-		it('should ignore matchers that are not an object', function () {
-			var m = [{
-				pattern: '\d+',
-				fields: ['test']
-			},
-			'invalid'];
+    it('should remove matchers that do not have a regexp RegExp property or a pattern string property', function () {
+      const m = [
+        {
+          pattern: 'd+',
+          fields: ['test'],
+        },
+        {
+          regexp: new RegExp('d+'),
+          fields: ['issue'],
+        },
+        {
+          fields: ['issue'],
+        },
+      ];
 
-			var prepared = matchers.prepareMatchers(m);
-			prepared.length.should.equal(1);
-		});
-		
-		it('should allow a single matcher', function () {
-			var prepared = matchers.prepareMatchers({
-				pattern: '\s+',
-				fields: ['spaces']
-			});
-			prepared.should.be.instanceOf(Array);
-			prepared.length.should.equal(1);
-		});
-		
-		it('should return an empty array if nothing is passed in', function () {
-			var prepared = matchers.prepareMatchers();
-			prepared.should.be.instanceOf(Array);
-			prepared.length.should.equal(0);
-		})
-	});
+      const prepared = matchers.prepareMatchers(m);
+      prepared.length.should.equal(2);
+    });
+
+    it('should generate RegExp objects from a string pattern', function () {
+      const m = [
+        {
+          pattern: 'd+',
+          fields: ['issue'],
+        },
+      ];
+
+      const prepared = matchers.prepareMatchers(m);
+      prepared[0].regexp.should.be.instanceOf(RegExp);
+    });
+
+    it('should ignore matchers that are not an object', function () {
+      const m = [
+        {
+          pattern: 'd+',
+          fields: ['test'],
+        },
+        'invalid',
+      ];
+
+      const prepared = matchers.prepareMatchers(m);
+      prepared.length.should.equal(1);
+    });
+
+    it('should allow a single matcher', function () {
+      const prepared = matchers.prepareMatchers({
+        pattern: 's+',
+        fields: ['spaces'],
+      });
+      prepared.should.be.instanceOf(Array);
+      prepared.length.should.equal(1);
+    });
+
+    it('should return an empty array if nothing is passed in', function () {
+      const prepared = matchers.prepareMatchers();
+      prepared.should.be.instanceOf(Array);
+      prepared.length.should.equal(0);
+    });
+  });
 });
